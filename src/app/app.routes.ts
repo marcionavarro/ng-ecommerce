cd80e2eb8d104c57595f62012fb38c3f24bd9877
Marcio Navarro <marcionavarrodearaujo@gmail.com>
2025-11-11 00:01:20 -0300

    Project Setup & Layout (Part 1)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'products',
+  },
+  {
+    path: 'products',
+    loadComponent: () => import('./pages/products-grid/products-grid'),
+  },
+  {
+    path: 'whishlist',
+    loadComponent: () => import('./pages/my-wishlist/my-wishlist'),
+  },
+];
